e55cd766223c0178789ec99324e74041ec12801c
Hema Dalavayi <gayathri.dhs@gmail.com>
2024-02-15 19:09:26 -0600

    Made use of times to iterate through a loop 5 times

---
+++ prob10.rb
@@ -0,0 +1,15 @@
+'''
+Problem 10: Iterating with times
+Objective: Use the times method to execute a block of code a specified number of times.
+Task: Print "Ruby is fun!" five times using the times method.
+Example Usage and Expected Output:
+Ruby is fun!
+Ruby is fun!
+Ruby is fun!
+Ruby is fun!
+Ruby is fun!
+'''
+
+5.times do |i|
+puts "Ruby is fun!"
+end
