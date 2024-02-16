45b50c2851e5c12e53954ad98e91058e5828da11
Hema Dalavayi <gayathri.dhs@gmail.com>
2024-02-15 19:10:57 -0600

    Used inclusive range to print numbers

---
+++ prob11.rb
@@ -0,0 +1,17 @@
+'''
+Problem 11: Ranges and Loops
+Objective: Practice using ranges in
+loops to iterate over a sequence of values.
+Task: Use a range to print all numbers from 1 to 5,
+inclusive, on separate lines.
+Example Usage and Expected Output:
+1
+2
+3
+4
+5
+'''
+
+(1..5).each do |number|
+  puts number
+  end
